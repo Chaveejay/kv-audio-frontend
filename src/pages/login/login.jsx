59e18222ec257240c76920696e2f4eda7e3b3170
Chaveejay <chawwa53@gmail.com>
2025-02-16 00:49:40 +0530

    Login frontend ✔️

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+export default function Login(){
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+
+    function handleOnSubmit(e){
+        e.preventDefault();
+        console.log(email,password);
+    }
+    return(
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm">
+        <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
+        
+        
+        <form className="space-y-4" onSubmit={handleOnSubmit}>
+          <div>
+          
+            <label htmlFor="username" className="block text-sm font-medium text-gray-700">Email</label>
+            <input 
+              type="text" 
+              id="username" 
+              value={email}
+              onChange={(e)=>setEmail(e.target.value)}
+              className="mt-2 w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              placeholder="Enter your email"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+            <input 
+              type="password" 
+              id="password" 
+              value={password}
+              onChange={(e)=>setPassword(e.target.value)}
+              className="mt-2 w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              placeholder="Enter your password"
+            />
+          </div>
+
+          <div className="flex justify-center items-center mt-4">
+            <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 w-full cursor-pointer">
+              Login
+            </button>
+          </div>
+          
+          <div className="mt-2 text-center">
+            <button className="text-blue-500 hover:underline cursor-pointer">
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+    )
+}
