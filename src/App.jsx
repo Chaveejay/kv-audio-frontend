68b88ccabf64d0081e8bcaf04df2c6dbe1d5ac72
Chaveejay <chawwa53@gmail.com>
2025-02-16 23:24:14 +0530

    Login function implementation ✔️

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './pages/login/login'
+import { Toaster } from 'react-hot-toast'
+import User from './pages/user/user'
+import Admin from './pages/admin/admin'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
+    <Toaster/>
     <Routes path = "/">
       <Route path="/" element={<Login/>}/>
+      <Route path="/user" element={<User/>}/>
+      <Route path="/admin" element={<Admin/>}/>
+      
 
     </Routes>
     </BrowserRouter>
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,15 +1,40 @@
+import axios from "axios";
 import { useState } from "react";
-
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 export default function Login(){
     const [email,setEmail] = useState("");
     const [password,setPassword] = useState("");
 
-    function handleOnSubmit(e){
-        e.preventDefault();
-        console.log(email,password);
+    const navigate = useNavigate();
+
+    async function handleOnSubmit(e){
+      e.preventDefault();
+      console.log(email,password)
+  try{
+    const res =await axios.post("http://localhost:3000/api/users/login",{
+        email: email,
+        password :password
+    })
+    console.log(res)
+    toast.success(res.data.message);
+    const user =res.data.user
+    localStorage.setItem("token",res.data.token)
+
+    if(user.role === "admin"){
+      navigate("/admin")
+    }else{
+      navigate("/user")
     }
+  }
+  catch(err){
+    toast.error(err.response.data.error)
+  }
+      
+  }
     return(
         <div className="flex justify-center items-center min-h-screen bg-gray-100">
+          <title>Login</title>
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm">
         <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
         
